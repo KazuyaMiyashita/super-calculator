224fe2b5ca688eb3fba633b548705c46d37492d6
mi12cp <miy1924@gmail.com>
2019-03-05 05:12:19 +0900

    ネストがなっていない時に対応するためOptionにした

--- src/main/scala/supercalc/Parser.scala
+++ src/main/scala/supercalc/Parser.scala
@@ -1,5 +1,7 @@
 package supercalc
 
+import scala.util.{Try, Success, Failure}
+
 object Parser {
 
   def parse(tokens: List[Token]): BinaryTree[Token] = {
@@ -18,18 +20,23 @@ object Parser {
    * @params tokens: ex. List(Number(1), ParenOpen, Number(2), Number(3), ParenClose)
    * @return ex. List(Number(1), List(Number(2), Number(3))
    */
-  def nest(tokens: List[Token]): List[Any] = {
+  def nest(tokens: List[Token]): Option[List[Any]] = {
 
-    def proc(nest: List[Any], remain: List[Token]): (List[Any], List[Token]) = {
-      if (remain.isEmpty) (nest, Nil)
-      else {
+    def proc(nest: List[Any], remain: List[Token], indent: Int): (List[Any], List[Token], Int) = {
+      if (remain.isEmpty) {
+        if (indent == 0) (nest, Nil, 0)
+        else throw new Exception("括弧の対応がおかしい")
+      } else {
         remain.head match {
           case ParenOpen => {
-            val (n, r) = proc(Nil, remain.tail)
-            proc(n ::: nest, r)
+            val (n, r, i) = proc(Nil, remain.tail, indent + 1)
+            proc(n ::: nest, r, i)
           }
-          case ParenClose => proc(nest :: Nil, remain.tail)
-          case t => proc(t :: nest, remain.tail)
+          case ParenClose => {
+            if (indent > 0) proc(nest :: Nil, remain.tail, indent - 1)
+            else throw new Exception("括弧の対応がおかしい")
+          }
+          case t => proc(t :: nest, remain.tail, indent)
         }
       }
     }
@@ -38,8 +45,13 @@ object Parser {
       nest.reverse.map { n => if (n.isInstanceOf[List[Any]]) reverse(n.asInstanceOf[List[Any]]) else n }
     }
 
-    val (nest, _) = proc(Nil, tokens)
-    reverse(nest)
+    Try {
+      val (nest, _, _) = proc(Nil, tokens, 0)
+      nest
+    } match {
+      case Success(n) => Some(reverse(n))
+      case Failure(e) => None
+    }
   }
 
 }
