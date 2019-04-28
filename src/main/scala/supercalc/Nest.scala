472dbfafc4d1ad76e4cc568628ffdac7591a464f
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-04-29 01:07:45 +0900

    Nest.scalaの修正

--- src/main/scala/supercalc/Nest.scala
+++ src/main/scala/supercalc/Nest.scala
@@ -1,27 +1,45 @@
 package supercalc
 
-sealed trait Nest[+T]
-
-case object NestNil extends Nest[Nothing]
-case class NestLeaf[T](value: T) extends Nest[T]
-case class NestList[T](value: Nest[T]*) extends Nest[T] {
-  override def toString = value.mkString("NestList(", ", ", ")")
+sealed trait Nest[+T] {
+  def reverse: Nest[T]
 }
 
-// type Not[A] = A => Nothing
-// type Or[A, B] = Not[Not[A] with Not[B]]
-
-// case object NestNil extends Nest[Nothing]
-// case class NestLeaf[T](value: T) extends Nest[T]
-// case class NestList[T, A](value: A*)(implicit ev: Not[Not[A]] <:< Or[T, Nest[T]]) extends Nest[T] {
-//   override def toString = value.mkString("NestList(", ", ", ")")
-// }
+case object NestNil extends Nest[Nothing] {
+  def reverse: Nest[Nothing] = this
+}
+case class NestLeaf[T](code: T) extends Nest[T] {
+  def reverse: Nest[T] = this
+}
+case class NestList[T](elements: Nest[T]*) extends Nest[T] {
+  def reverse: Nest[T] = NestList(elements.reverse.map(_.reverse):_*)
+  override def toString = elements.mkString("NestList(", ", ", ")")
+}
 
 object Nest {
+
   def apply() = NestNil
   def apply[T](value: T*) = {
     if (value.length <= 1) NestLeaf(value(0))
     else NestList(value.map({v => NestLeaf(v)}): _*)
   }
   def apply[T](value: Nest[T]*) = NestList(value: _*)
+
+  def group[T](tokens: List[T], parenOpen: T, parenClose: T): List[Nest[T]] = {
+    def process(nest: List[Nest[T]], acc: List[Nest[T]], remain: List[T]): (List[Nest[T]], List[Nest[T]], List[T]) = {
+      remain match {
+        case Nil => (nest, Nil, Nil)
+        case `parenOpen`::rest =>
+          val (n, a, r) = process(Nil, nest, rest)
+          process(n ::: acc, a, r)
+        case `parenClose`::rest =>
+          process(NestList(nest:_*) :: Nil ::: acc, Nil, rest)
+        case t::rest =>
+          process(NestLeaf(t) :: nest, acc, rest)
+      }
+    }
+  
+    val (nest, _, _) = process(Nil, Nil, tokens)
+    nest.reverse
+  }
+
 }
